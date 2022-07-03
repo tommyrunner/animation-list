b6675e455636406d48d5db2612edecc95f6500d3
tommyrunner <1223758238@qq.com>
2022-07-03 11:58:30 +0800

    v1.0.0 animationList

---
+++ README.md
@@ -0,0 +1,170 @@
+# animation-list
+
+## 简介
+
+> + animation-list 基于vue，支持vue3.0
+> + animation-list 以vue指令方式获取dom中每个子元素（可以获取更深层children），并以动画渲染子元素。
+
+## 引用
+
++ 安装
+
+```shell
+npm i animation-list
+```
+
++ 引用
+
+```js
+// 引用animationList
+import animationList from "animation-list";
+// 引用animationList默认动画
+import "animation-list/index.css";
+Vue.use(animationList);
+```
+
+## 使用
+
++ 例子
+
+```vue
+<template>
+  <div class="list" v-list-show="'right-left'">
+    <div class="item" v-for="item in 8" :key="item">{{ item }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style  scoped>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  line-height: 50px;
+}
+.list .item {
+  width: 100px;
+  height: 50px;
+  border: 1px solid gainsboro;
+  margin: 5px;
+  box-shadow: 4px 5px 9px gainsboro;
+  color: gray;
+  border-radius: 4px;
+}
+</style>
+
+```
+
+> + 只需要在 标签中假如指定  **v-list-show** 即可
+
+## 参数
+
+### 指令
+
++ v-list-show
+  + 浅层获取子元素，动画渲染
++ v-list-show-deep
+  + 深层获取子元素，动画渲染
+
+### 默认动画
+
+| 动画       | 效果       | 备注               |
+| ---------- | ---------- | ------------------ |
+| bottom-top | 从下向上   |                    |
+| top-bottom | 从上向下   |                    |
+| right-left | 从右到左   | 默认               |
+| left-right | 从左到右   |                    |
+| small-big  | 从小到大   |                    |
+| big-small  | 从大到小   |                    |
+| custom     | 自定义动画 | 根据例子自定义动画 |
+
+## 自定义动画
+
+```vue
+<template>
+<div class="list" v-list-show="'custom'">
+    <div class="item" v-for="item in 8" :key="item">{{ item }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {},
+        data() {
+            return {};
+        },
+    };
+</script>
+
+<style  scoped>
+    .list {
+        display: flex;
+        flex-wrap: wrap;
+        text-align: center;
+        line-height: 50px;
+    }
+    .list .item {
+        width: 100px;
+        height: 50px;
+        border: 1px solid gainsboro;
+        margin: 5px;
+        box-shadow: 4px 5px 9px gainsboro;
+        color: gray;
+        border-radius: 4px;
+    }
+	/** 自定义动画部分(必须) **/
+    .ls-custom {
+        opacity: 0 !important;
+    }
+    .ls-custom {
+        animation: ls-custom ease 0.38s !important;
+    }
+    @keyframes ls-custom {
+        0% {
+            opacity: 0;
+            transform: translateY(100%) scale(0.6);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0%) scale(1);
+        }
+    }
+</style>
+```
+
+> + 告诉 v-list-show 自定义 **custom**
+> + 在 style 样式加入
+>   + ls-custom 初始化
+>   + ls-custom 动画开始
+>   + keyframes 动画
+
+## hooks生命周期
+
+```js
+mounted() {
+    this.$animationListHooks.start = () => {
+        console.log("动画开始");
+    };
+    this.$animationListHooks.update = (params) => {
+        console.log("动画跳动", params);
+    };
+    this.$animationListHooks.end = () => {
+        console.log("动画结束");
+    };
+}
+```
+
+> 主要函数抛在 **$animationListHooks** 中 
+
+## 附
+
++ 暂无
---
+++ index.js
@@ -0,0 +1,167 @@
+// 兼容浏览器-监听animation
+function getOnTransitionEvent() {
+  let el = document.createElement("surface"),
+    // 利用transition来识别浏览器兼容animationend
+    animations = {
+      transition: "animationend",
+      OnTransiTion: "onAnimationEnd",
+      webkittransition: "weikitanimationend",
+      WebkitTransiTion: "weikitAnimationEnd",
+    };
+
+  for (let key in animations) {
+    if (el.style[key] !== undefined) {
+      return animations[key];
+    }
+  }
+}
+let animationList = {
+  install: function (Vue) {
+    let callTime = true, // 防止多次触发
+      $el = null, // 当前元素
+      elChildern = [], // 当前元素的子元素
+      $deep = false, // 是否深层动画
+      _hooks = {},
+      // 默认动画 custom：自定义
+      AnimationTypes = ["bottom-top", "top-bottom", "right-left", "left-right", "small-big", "big-small", "custom"],
+      animationType = "right-left", // 默认动画
+      _$HooksName = "$animationListHooks", // 抛出hooks
+      animationEventName = getOnTransitionEvent(); // 获取动画回函数调名
+    Vue.mixin({
+      methods: {
+        // 抛出动画触发函数
+        $animationListShow() {
+          if ($el && callTime) {
+            // 不能多次触发
+            callTime = false;
+            // 初始化
+            elChildern = [];
+            // 获取动画元素
+            takeElChildern(Array.from($el.children), elChildern, $deep);
+            if (elChildern.length > 0) {
+              callHooks("start");
+              // 开始动画
+              callAnima(0, elChildern);
+            }
+          }
+        },
+      },
+      created() {
+        // 界面开始初始化数据
+        callTime = true;
+        $el = null;
+        elChildern = [];
+        $deep = false;
+        _hooks = {};
+        // 初始化Hooks
+        initHooks(this);
+      },
+      mounted() {
+        this.$nextTick(() => {
+          // 启动动画
+          this.$animationListShow();
+        });
+      },
+    });
+    // 浅层
+    Vue.directive("list-show", {
+      bind(el, binding, vnode) {
+        getElConfig(el, binding, false);
+      },
+      // 兼容vue3.0
+      beforeMount(el, binding, vnode) {
+        getElConfig(el, binding, false);
+      },
+    });
+    // 深层
+    Vue.directive("list-show-deep", {
+      bind(el, binding, vnode) {
+        getElConfig(el, binding, true);
+      },
+      // 兼容vue3.0
+      beforeMount(el, binding, vnode) {
+        getElConfig(el, binding, true);
+      },
+    });
+    /**
+     * 获取参数
+     * @param {*} el
+     * @param {*} binding
+     */
+    function getElConfig(el, binding, deep) {
+      $el = el;
+      $deep = deep;
+      binding.value && AnimationTypes.includes(binding.value) && (animationType = binding.value);
+    }
+    /**
+     * 开始动画
+     * @param {*} listIndex 动画元素的小标
+     * @param {*} list 动画元素集合
+     * @returns
+     */
+    function callAnima(listIndex, list) {
+      if (listIndex > list.length - 1) {
+        callHooks("end");
+        callTime = true;
+        return;
+      }
+      let el = list[listIndex];
+      callHooks("update", { index: listIndex, el });
+      animationEventName &&
+        el.addEventListener(
+          animationEventName,
+          function evnet() {
+            el.classList.remove("ls-init");
+            el.classList.remove(`ls-${animationType}`);
+            el.removeEventListener(animationEventName, evnet, false); //销毁事件
+            callAnima(++listIndex, list);
+          },
+          false
+        );
+      el.classList.add(`ls-${animationType}`);
+    }
+
+    /**
+     * 取出需要动画的元素
+     * @param {*} el // 需要动画的元素
+     */
+    function takeElChildern(els, values, deep) {
+      if (els.length > 0) {
+        if (deep) {
+          for (let el of els) {
+            if (el.children && el.children.length > 0) {
+              takeElChildern(Array.from(el.children), values, deep);
+            } else {
+              el.classList && el.classList.add("ls-init");
+              values.push(el);
+            }
+          }
+        } else {
+          for (let el of els) {
+            el.classList && el.classList.add("ls-init");
+            values.push(el);
+          }
+        }
+      }
+    }
+    // ---- hooks
+    function initHooks($vue) {
+      if (!$vue[_$HooksName]) _hooks = $vue[_$HooksName] = { start: () => {}, update: () => {}, end: () => {} };
+      if (getDataType($vue[_$HooksName]) === "object") {
+        Object.keys($vue[_$HooksName]).forEach((key) => {
+          if (getDataType($vue[_$HooksName][key]) === "function") {
+            _hooks[key] = $vue[_$HooksName][key];
+          }
+        });
+      }
+    }
+    function callHooks(event, params) {
+      _hooks[event] && _hooks[event](params);
+    }
+    // ---- utils
+    function getDataType(data) {
+      return String(Object.prototype.toString.call(data).split(" ")[1].split("]")[0]).toLowerCase();
+    }
+  },
+};
+export default animationList;
